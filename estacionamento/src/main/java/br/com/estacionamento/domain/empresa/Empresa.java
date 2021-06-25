46775121826968a1e991476526cff6e0e9e09a43
Enzo Carvalho <enzocsantos18@gmail.com>
2021-06-25 14:45:36 -0300

    fix: Deletar Empresa

--- estacionamento/src/main/java/br/com/estacionamento/domain/empresa/Empresa.java
+++ estacionamento/src/main/java/br/com/estacionamento/domain/empresa/Empresa.java
@@ -1,5 +1,8 @@
 package br.com.estacionamento.domain.empresa;
 
+import br.com.estacionamento.domain.estacionamento.Estacionamento;
+import br.com.estacionamento.domain.usuario.Usuario;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
@@ -16,6 +19,14 @@ public class Empresa {
     @OneToMany (mappedBy = "empresa", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @Fetch(value = FetchMode.SUBSELECT)
     private List<Telefone> telefone = new ArrayList<>();
+    @JsonIgnore
+    @OneToMany (mappedBy = "empresa", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @Fetch(value = FetchMode.SUBSELECT)
+    private List<Usuario> usuario = new ArrayList<>();
+    @JsonIgnore
+    @OneToMany (mappedBy = "empresa", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @Fetch(value = FetchMode.SUBSELECT)
+    private List<Estacionamento> estacionamentos = new ArrayList<>();
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Endereco endereco;
 
