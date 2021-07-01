ebd647cbaac88c66b5f22e3d122f2deb51ce55bb
Enzo Carvalho <enzocsantos18@gmail.com>
2021-07-01 09:47:39 -0300

    doc: inserts iniciais banco

---
+++ inserts.sql
@@ -0,0 +1,40 @@
+insert into tipo_usuario values (1,"admin");
+insert into tipo_usuario values (2,"admin_estacionamento");
+insert into tipo_usuario values (3,"funcionario");
+
+insert into tipo_veiculo values (1,"Carro");
+insert into tipo_veiculo values (2,"Moto");
+
+insert into fabricante values (1,"Chevrolet");
+insert into modelo values (1, "Onix", 1, 1);
+
+insert into fabricante values (2,"Fiat");
+insert into modelo values (2, "Toro", 2, 1);
+
+insert into fabricante values (3,"Ford");
+insert into modelo values (3, "Ka", 3, 1);
+
+insert into fabricante values (4,"Volkswagen");
+insert into modelo values (4, "Fox", 4, 1);
+
+insert into fabricante values (5,"BMW");
+insert into modelo values (5, "X6", 5, 1);
+
+insert into fabricante values (6,"Audi");
+insert into modelo values (6, "m4", 6, 1);
+
+insert into fabricante values (7,"Mercedes");
+insert into modelo values (7, "c180 coupé", 7, 1);
+
+insert into fabricante values (8,"Hyundai");
+insert into modelo values (8, "Creta", 8, 1);
+
+insert into fabricante values (9,"Toyota");
+insert into modelo values (9, "Corolla", 9, 1);
+
+insert into fabricante values (10,"Honda");
+insert into modelo values (10, "Civic", 10, 1);
+insert into modelo values (11, "CG", 10, 2);
+
+insert into fabricante values (11,"Triumph");
+insert into modelo values (12, "Speed Twin", 11, 2);
